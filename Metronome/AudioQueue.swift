f30267fbaed41a57a139b14fc2f07dd1c0f7f967
Scott Austin <scott.austin7@gmail.com>
2016-03-27 09:04:40 -0400

    Initial commit

---
+++ Metronome/AudioQueue.swift
@@ -0,0 +1,16 @@
+//
+//  AudioQueue.swift
+//  Metronome
+//
+//  Created by Brittany Austin on 3/24/16.
+//  Copyright © 2016 Europa Software. All rights reserved.
+//
+
+import Foundation
+import CoreAudio
+import AudioToolbox
+
+struct myAWStruct
+{
+    
+}
--- Metronome/ViewController.swift
+++ Metronome/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    var tempo = 120.0
+    var subdivision = 3.0
+    var timer: NSTimer?
+    var currentNote = 0
+    
+    var audioPlayer: AVAudioPlayer?
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let filePath = NSBundle.mainBundle().pathForResource("beep-07", ofType: "wav")
+        let fileURL = NSURL(fileURLWithPath: filePath!)
+        
+        do
+        {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: fileURL)
+        }
+        catch
+        {
+            NSLog("\(error)")
+        }
+        
+        
+        timer = NSTimer(timeInterval: (60 / tempo) / subdivision,
+                        target: self,
+                        selector: #selector(ViewController.playSound),
+                        userInfo: nil,
+                        repeats: true)
+        NSRunLoop.currentRunLoop().addTimer(timer!, forMode: NSRunLoopCommonModes)
+        timer?.fire()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func playSound()
+    {
+        NSLog("\(NSDate())")
+        audioPlayer?.play()
+    }
 
 }
 
